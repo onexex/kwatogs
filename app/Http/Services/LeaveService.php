961eccab8d844738bab4f50e3b322c44caffe6d1
marjun.ladag@gmail.com <marjun.ladag@gmail.com>
2026-03-09 11:26:34 +0800

    feat: enhance leave approval process by updating leave balance upon CFO approval

--- app/Http/Services/LeaveService.php
+++ app/Http/Services/LeaveService.php
@@ -3,7 +3,10 @@
 namespace App\Http\Services;
 
 use App\Enums\LeaveStatusEnum;
+use App\Models\EmpDetail;
 use App\Models\Leave;
+use App\Models\LeaveCreditAllocation;
+use App\Models\leavevalidationModel;
 
 class LeaveService
 {
@@ -16,6 +19,38 @@ class LeaveService
             $leave->leaveDetails()->update([
                 'status' => $status
             ]);
+
+            if ($status == LeaveStatusEnum::APPROVEDBYCFO->name) {
+                $leaveType = $leave->leaveType;
+
+                if ($leaveType) {
+                    $employeeDetail = EmpDetail::where('empID', $leave->employee_id)
+                        ->first();
+                    
+                    if ($employeeDetail) {
+                        $leaveValidation = leavevalidationModel::where('leave_type', $leaveType->id)
+                            ->where('compID', $employeeDetail->empCompID)
+                            ->first();
+                        if ($leaveValidation->pre_allocated == 1) {
+                            
+                            $preAllocatedLeave = LeaveCreditAllocation::where('employee_id', $employeeDetail->empID)
+                                ->where('leavetype_id', $leaveType->id)
+                                ->where('year', now()->year)
+                                ->first();
+
+                            if ($preAllocatedLeave) {
+                                $balance = $preAllocatedLeave->balance;
+
+                                $leaveDuration = $leave->total_hrs / 8;
+
+                                $preAllocatedLeave->balance = $balance - $leaveDuration;
+                                $preAllocatedLeave->save();
+                            }
+                        }
+                    }
+
+                }
+            }
 
             $leave->status = $status;
             $leave->save();
