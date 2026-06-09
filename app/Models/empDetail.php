56c5587ea3375c28605a714a888a9ccd2b8bbe63
onexex <rgemana@gmail.com>
2026-06-09 16:12:25 +0800

    fix: correct class name casing for empDetail model

--- app/Models/empDetail.php
+++ app/Models/empDetail.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class EmpDetail extends Model
+class empDetail extends Model
 {
     use HasFactory;
 
